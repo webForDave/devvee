7020bc19cbc785c84b01c87ae747eae5fab621dc
David <olalekaneniola203@gmail.com>
2023-07-29 14:28:58 -0700

    Worked on the header and navigation section and added a few packages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
+import Header from './Components/Header'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom';
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+
+        <Navbar />
+
+        <Switch>
+
+            <Route exact path="/">
+              <Header />
+            </Route>
+
+          </Switch>
+
+      </Router>
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MdMenu } from 'react-icons/md';
+import { GrClose } from 'react-icons/gr';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav
+      className={`navbar flex justify-between items-center py-2 px-4 lg:px-8 h-auto font-[cambria] font-bold ${isOpen ? 'bg-white h-full' : 'bg-transparent'} text-black ${isOpen ? 'flex-col' : 'block'} ${isOpen ? '' : 'h-10 lg:h-auto'}`}>
+
+      <ul
+        className={`flex items-center space-x-6 lg:space-x-8 ${isOpen ? 'flex-col space-y-4' : 'hidden lg:flex'}`}>
+        <li>
+          <Link to="/about">ABOUT US</Link>
+        </li>
+        <li>
+          <Link to="/services">WE OFFER</Link>
+        </li>
+        <li>
+          <Link to="/why">WHY US</Link>
+        </li>
+        <li>
+          <Link to="/chances">GET YOUR CHANCES</Link>
+        </li>
+        <li>
+          <Link to="/testimonials">TESTIMONIALS</Link>
+        </li>
+      </ul>
+
+      {/* Contact Info and Button */}
+      <div className="flex items-center">
+        <a href="tel:+2349077565788" className="hidden lg:block mr-6">
+          +2349077565788
+        </a>
+        <button className="hidden lg:block px-4 py-2 rounded-[25px] bg-orange-500">
+          ORDER NOW
+        </button>
+
+        {/* Mobile Menu Icon */}
+        <div
+          className="lg:hidden ml-6 cursor-pointer absolute right-8 top-1"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <GrClose size={20} /> : <MdMenu size={30} />}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
